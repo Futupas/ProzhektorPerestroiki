4a47e86332863d34bc08b01bb7a79a8f7fc005bf
Olexandr Panov <futupas@gmail.com>
2020-06-15 00:22:29 +0300

    websocket test 2

--- index.js
+++ index.js
@@ -42,42 +42,18 @@ body{
 `;
 
 
-
-const http = require('http');
-const ws = require('ws');
-
-const wss = new ws.Server({noServer: true});
-
-function accept(req, res) {
-  // все входящие запросы должны использовать websockets
-  if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
-    res.end();
-    return;
-  }
-
-  // может быть заголовок Connection: keep-alive, Upgrade
-  if (!req.headers.connection.match(/\bupgrade\b/i)) {
-    res.end();
-    return;
-  }
-
-  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
-}
-
-function onConnect(ws) {
-  ws.on('message', function (message) {
-    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Гость";
-    ws.send(`Привет с сервера, ${name}!`);
-
-    setTimeout(() => ws.close(1000, "Пока!"), 5000);
+const express = require('express');
+const WebSocket = require('ws'); 
+const socketServer = new WebSocket.Server({port: 3030});
+socketServer.on('connection', (socketClient) => {
+  console.log('connected');
+  console.log('client Set length: ', socketServer.clients.size);
+  socketClient.on('close', (socketClient) => {
+    console.log('closed');
+    console.log('Number of clients: ', socketServer.clients.size);
   });
-}
+});
 
-if (!module.parent) {
-  http.createServer(accept).listen(8080);
-} else {
-  exports.accept = accept;
-}
 
 
 
