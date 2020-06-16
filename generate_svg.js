4bb09be55c25cc2bc10d2894882a2f1635afa958
Olexandr Panov <futupas@gmail.com>
2020-06-16 13:31:20 +0300

    fix bugs: class field definition inside constructor

--- generate_svg.js
+++ generate_svg.js
@@ -25,14 +25,11 @@ let raySegments = [];
 
 
 class PathElement {
-    styleStroke = '#000';
-    styleStrokeWidth = '1px';
-    styleFill = '#000';
-
-    d = '';
-
     constructor () {
-
+        this.styleStroke = '#000';
+        this.styleStrokeWidth = '1px';
+        this.styleFill = '#000';
+        this.d = '';
     }
 
     /**
@@ -60,10 +57,10 @@ class PathElement {
 
 
 class LineSegment {
-    x1 = 0;
-    x2 = 0;
-    y1 = 0;
-    y2 = 0;
+    // x1 = 0;
+    // x2 = 0;
+    // y1 = 0;
+    // y2 = 0;
 
     constructor(x1, y1, x2, y2) {
         this.x1 = x1;
