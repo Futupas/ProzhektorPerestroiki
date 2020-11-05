ab6e25c54e6c784bb359f1c90db8f11dedab79e5
Olexandr Panov <futupas@gmail.com>
2020-11-05 12:06:13 +0200

    commit

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ let db = pgp(process.env.DATABASE_URL);
 const app = express()
 .use(express.static('static'))
 .use(express.json())
+.get('/', (req, res) => {
+    let delta = Math.random() * (Math.PI / 16); // about 0..11deg
+
+    let options = {
+        windowSize: { width: 1000, height: 1000 },
+        shotSize: { width: 'window', height: 'window' },
+        siteType: 'html'
+    };
+    
+    db.one(`UPDATE "angle_table" SET "value" = mod(CAST("value" + ${delta} AS NUMERIC), CAST(PI() AS NUMERIC)) WHERE "key" = 0 RETURNING "value"`)
+        .then(function (data) {
+            var webshot = require('webshot');
+            webshot(generateSvg.generateHTMLString(data.value * 1.0), 'static/picture.png', options, function(err) {
+                wss.clients.forEach((client) => {
+                    client.send('c');
+                });
+                res.sendFile('static/main.html', noCacheOptions);
+            });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('error ' + error);
+        });
+
+})
 .get('/ua', (req, res) => { 
     console.log('fff');   
     let delta = Math.random() * (Math.PI / 16); // about 0..11deg
@@ -37,32 +63,6 @@ const app = express()
                     client.send('c');
                 });
                 res.sendFile('static/ua.html', noCacheOptions);
-            });
-        })
-        .catch(function (error) {
-            console.log('ERROR:', error);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('error ' + error);
-        });
-
-})
-.get('/', (req, res) => {
-    let delta = Math.random() * (Math.PI / 16); // about 0..11deg
-
-    let options = {
-        windowSize: { width: 1000, height: 1000 },
-        shotSize: { width: 'window', height: 'window' },
-        siteType: 'html'
-    };
-    
-    db.one(`UPDATE "angle_table" SET "value" = mod(CAST("value" + ${delta} AS NUMERIC), CAST(PI() AS NUMERIC)) WHERE "key" = 0 RETURNING "value"`)
-        .then(function (data) {
-            var webshot = require('webshot');
-            webshot(generateSvg.generateHTMLString(data.value * 1.0), 'static/picture.png', options, function(err) {
-                wss.clients.forEach((client) => {
-                    client.send('c');
-                });
-                res.sendFile('static/main.html', noCacheOptions);
             });
         })
         .catch(function (error) {
